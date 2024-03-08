9ec874e739fe8497fc2fed4769d0b393da29a994
Артур Захаркин <zaharkin.artur75@gmail.com>
2024-03-08 23:40:12 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Simulation_Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btStart
+            // 
+            this.btStart.BackColor = System.Drawing.Color.Transparent;
+            this.btStart.Location = new System.Drawing.Point(22, 19);
+            this.btStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(86, 22);
+            this.btStart.TabIndex = 0;
+            this.btStart.Text = "Старт";
+            this.btStart.UseVisualStyleBackColor = false;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(394, 19);
+            this.btStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(86, 22);
+            this.btStop.TabIndex = 1;
+            this.btStop.Text = "Стоп";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox.Location = new System.Drawing.Point(22, 60);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(459, 358);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(502, 440);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_Lab_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics graphics;
+        bool[,] field;
+        int cols;
+        int rows;
+        int Res = 10;
+        int Den = 5;
+
+        private int Neighbours(int x, int y) //количество живых соседей
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + j + rows) % rows;
+                    var Check = col == x && row == y;
+                    var life = field[col, row];
+                    if (life && !Check) count++;
+                }
+            return count;
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) return;
+
+            rows = pictureBox.Height / Res; //создание поля
+            cols = pictureBox.Width / Res;
+
+            field = new bool[cols, rows];
+
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+                for (int y = 0; y < rows; y++)
+                    field[x, y] = random.Next(Den) == 0;
+
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(pictureBox.Image);
+            timer1.Start();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled) return;
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Gray);
+
+            var newField = new bool[cols, rows];
+
+            //алгоритм (если у мертвой клетки, 3 соседа, то она оживает, иначе ничего не меняется)
+            for (int x = 0; x < cols; x++) 
+                for (int y = 0; y < rows; y++)
+                {
+                    var neigboursCount = Neighbours(x, y);
+                    bool life = field[x, y];
+
+                    if (!life && neigboursCount == 3) newField[x, y] = true;
+                    else if (life && (neigboursCount < 2 || neigboursCount > 3)) newField[x, y] = false;
+                    else newField[x, y] = field[x, y];
+
+                    if (life)
+                        graphics.FillRectangle(Brushes.Black, x * Res, y * Res, Res - 1, Res - 1);
+                }
+            field = newField;
+            pictureBox.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
